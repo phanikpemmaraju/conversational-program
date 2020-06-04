3a657cff13a0e4d0efd82a71b0a46153cfc13228
Phani Pemmaraju <phani16@gmail.com>
2020-06-04 20:22:15 +0100

    Upgraded to stable spring boot release.

--- src/main/java/org/legacy/it/services/FacadeService.java
+++ src/main/java/org/legacy/it/services/FacadeService.java
@@ -9,10 +9,7 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 @Service
 @Slf4j
